dd3859680298335e03ea09fa99bdf24a603469fd
Lubega <beckhamlubega@gmail.com>
2026-03-21 15:38:06 +0300

    added more images

--- app/src/main/java/ug/ac/ndejje/welcome/StudentProvider.kt
+++ app/src/main/java/ug/ac/ndejje/welcome/StudentProvider.kt
@@ -6,7 +6,11 @@ class StudentProvider {
             Student(1, "Akello Stellamaris", "24/2/314/01", "BIT", R.drawable.female_1, true),
             Student(2, "Kirya James", "24/2/314/02", "BCS", R.drawable.male_1, false),
             Student(3, "Mbabazi Joan", "24/2/314/03", "BIT", R.drawable.female_2, true),
-            Student(4, "Kato Johnmary", "24/2/314/04", "BSE", R.drawable.male_2, true)
+            Student(4, "Kato Johnmary", "24/2/314/04", "BSE", R.drawable.male_2, true),
+            Student(5, "Nalwoga Proscovia", "24/2/314/05", "BIT", R.drawable.female_3, false),
+            Student(6, "Mukasa Joseph", "24/2/314/06", "BSE", R.drawable.male_3, true),
+            Student(7, "Acan Grace", "24/2/314/07", "BIT", R.drawable.female_4, true),
+            Student(8, "Kavuma David", "24/2/314/08", "BCS", R.drawable.male_4, false)
         )
     }
 }
